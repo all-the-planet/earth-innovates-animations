a8bec82624ab936de5737575c9e531cd2172a113
stefian <v_ianta@yahoo.com>
2019-06-19 21:47:23 -0400

    added tweened w duration and easing for progress bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script>
     import { writable } from "svelte/store";
+    import { tweened } from "svelte/motion";
+    import { cubicIn } from "svelte/easing";
 
-    const progress = writable(0);
+
+    const progress = tweened(0, {
+        delay: 0,
+        duration: 400,   // default = 400
+        easing: cubicIn
+    });
+
+    setTimeout(() => {
+        progress.set(0.5);
+    }, 1500);
 </script>
 
 <progress value={$progress}></progress>
