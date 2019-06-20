1b05b198aa54d90b83d0c19c4be69e4af2597ac7
stefian <v_ianta@yahoo.com>
2019-06-20 18:26:09 -0400

    added discard function to the playReal function

--- src/App.svelte
+++ src/App.svelte
@@ -81,12 +81,15 @@
     boxes = [...boxes, { id: id, commit: commit }];
   }
   function discardBox(value) {
-      boxes = boxes.filter(el => el.id !== value);
+    boxes = boxes.filter(el => el.id !== value);
   }
 
   let halves = [{ id: 0, commit: Math.floor(5 * Math.random()), real: 0 }];
   function addHalf(id, commit, real) {
     halves = [...halves, { id: id, commit: commit, real: real }];
+  }
+  function discardHalf(value) {
+    halves = halves.filter(el => el.id !== value);
   }
 
   // add days div squares with a short delay
@@ -101,12 +104,12 @@
         i++;
         addBox(i, Math.floor(5 * Math.random()));
       }
-      // remove first elements after buffer lenght reached 
+      // remove first elements after buffer lenght reached
       if (i >= 335) {
-          i++;
-          discardBox(i - 335);
+        i++;
+        discardBox(i - 335);
       }
-    }, 1);
+    }, 1);  // end of setInterval
 
     if (i >= 3 * 335) {
       return () => {
@@ -115,7 +118,7 @@
     }
   }
 
-    function playReal() {
+  function playReal() {
     let i = 0;
     let commits = 0;
     let normalized = 0;
@@ -123,19 +126,24 @@
       if (i < 335) {
         i++;
         commits = Math.floor(5 * Math.random());
-        normalized = Math.floor(norm[Math.floor((i/7))%48]*commits);
-        addHalf(i, commits, normalized > 0 ? 1 : 0 );
+        normalized = Math.floor(norm[Math.floor(i / 7) % 48] * commits);
+        addHalf(i, commits, normalized > 0 ? 1 : 0);
         // console.log(i/7, norm[(i/7)%48]);
       }
+      // remove first elements after buffer lenght reached
+      if (i >= 335) {
+        i++;
+        discardHalf(i - 335);
+      }
+
     }, 1);
 
-    if (i >= 335) {
+    if (i >= 3 * 335) {
       return () => {
         clearInterval(interval);
       };
     }
   }
-
 </script>
 
 <style>
@@ -181,7 +189,7 @@
 
 <!-- <input type="text" bind:this={boxInput}>
 <button on:click={addBox}>Add</button> -->
-<h1>Progressive Contributions (animated)</h1>
+<h1>Progressive Contributions & Discards</h1>
 
 <h2 on:click={play}>Yearly ~ 48 work weeks</h2>
 <section class="year">
