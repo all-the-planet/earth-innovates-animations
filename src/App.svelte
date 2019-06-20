5784e5c33f170426546fe699d6b114d24defc50a
stefian <v_ianta@yahoo.com>
2019-06-19 22:30:51 -0400

    added 3 boxes

--- src/App.svelte
+++ src/App.svelte
@@ -14,9 +14,25 @@
     setTimeout(() => {
         progress.set(0.5);
     }, 1500);
+
+    let boxes = ['Apples', 'Bananas', 'Oranges'];
 </script>
 
+<style>
+    div {
+        width: 10rem;
+        height: 10rem;
+        background: #ccc;
+        margin: 1rem;
+        box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+        border-radius: 5px;
+        padding: 1rem;
+    }
+</style>
+
 <!-- <progress value={$progress}></progress> -->
+<!-- <Spring /> -->
 
-<Spring />
-
+{#each boxes as box (box)} 
+    <div>{box}</div>
+{/each}
